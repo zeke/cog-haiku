be6e3c8bb391d6ac29edd6db2c694bc391a515c8
Zeke Sikelianos <zeke@sikelianos.com>
2022-02-16 19:24:49 -0800

    support progressive output
    this adds two new arguments to the Predict function `progressive` and `sleep`. If progressive is enabled, the resulting haiku is yielded one word at a time, sleeping between each word.

--- predict.py
+++ predict.py
@@ -1,4 +1,6 @@
 from cog import BasePredictor, Input
+from typing import Any
+import time
 import haiku
 import random
 
@@ -7,8 +9,22 @@ class Predictor(BasePredictor):
       self.haikus = haiku.load_all()
 
     def predict(self, 
-      seed: int = Input(description="An optional random seed", ge=0, default=None)) -> str:
-      if seed:
-        return self.haikus[seed % len(self.haikus)]
-      else:
-        return random.choice(self.haikus)
+      seed: int = Input(description="A seed to always return the same result (optional, defaults to a random integer)", ge=0, default=None),
+      progressive: bool = Input(description="Yield haiku words one at a time, sleepin between each word", default=False),
+      sleep: float = Input(description="Time to sleep between each word (when using progressive output)", default=0.5),
+      ) -> Any:
+        if seed:
+          haiku = self.haikus[seed % len(self.haikus)]
+        else:
+          haiku = random.choice(self.haikus)
+
+        print("seed: ", seed)
+        print("progressive: ", progressive)
+        print("sleep: ", sleep)
+
+        if progressive:
+          for word in haiku.split():
+            yield word
+            time.sleep(sleep)
+        else:
+          return haiku
