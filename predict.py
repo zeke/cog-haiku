184025cdb0289a6bf228e42838541ffdb427c103
Zeke Sikelianos <zeke@sikelianos.com>
2022-02-22 15:13:57 -0800

    support progressive output (#4)
    * support progressive output
    
    this adds two new arguments to the Predict function `progressive` and `sleep`. If progressive is enabled, the resulting haiku is yielded one word at a time, sleeping between each word.
    
    * define two different Predictors
    
    one for returning a full haiku, the other for yielding one word at a time.
    
    * add two predict stanzas to cog.yaml

--- predict.py
+++ predict.py
@@ -1,14 +1,43 @@
 from cog import BasePredictor, Input
+from typing import Generator
+import time
 import haiku
 import random
 
-class Predictor(BasePredictor):
+class HaikuBasePredictor(BasePredictor):
+    """
+    Base predictor from which Standard and Progressive predictors inherit
+    """
     def setup(self):
       self.haikus = haiku.load_all()
 
-    def predict(self, 
-      seed: int = Input(description="An optional random seed", ge=0, default=None)) -> str:
-      if seed:
+    def get_haiku(self, seed: int = None) -> str:
+      if seed and type(seed) == int:
         return self.haikus[seed % len(self.haikus)]
-      else:
-        return random.choice(self.haikus)
+      
+      return random.choice(self.haikus)
+
+class StandardPredictor(HaikuBasePredictor):
+    """
+    Return haiku as a single string.
+    """
+    def predict(self, 
+      seed: int = Input(description="A seed to always return the same result (optional)", ge=0, default=None)
+      ) -> str:
+        haiku = self.get_haiku(seed)
+        return haiku
+
+class ProgressivePredictor(HaikuBasePredictor):
+    """
+    Yield haiku as progressive output, one word at a time.
+    """
+    def predict(self, 
+      seed: int = Input(description="A seed to always return the same result (optional)", ge=0, default=None),
+      sleep: float = Input(description="Time to sleep between each word (when using progressive output)", default=0.1),
+      ) -> Generator[str, None, None]:
+
+        haiku = self.get_haiku(seed)
+
+        for word in haiku.split():
+          yield word
+          time.sleep(sleep)
