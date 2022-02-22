f61a53407d892fd604456ca893ac1931039cfa09
Zeke Sikelianos <zeke@sikelianos.com>
2022-02-22 15:03:34 -0800

    define two different Predictors
    one for returning a full haiku, the other for yielding one word at a time.

--- predict.py
+++ predict.py
@@ -1,30 +1,43 @@
 from cog import BasePredictor, Input
-from typing import Any
+from typing import Generator
 import time
 import haiku
 import random
 
-class Predictor(BasePredictor):
+class HaikuBasePredictor(BasePredictor):
+    """
+    Base predictor from which Standard and Progressive predictors inherit
+    """
     def setup(self):
       self.haikus = haiku.load_all()
 
+    def get_haiku(self, seed: int = None) -> str:
+      if seed and type(seed) == int:
+        return self.haikus[seed % len(self.haikus)]
+      
+      return random.choice(self.haikus)
+
+class StandardPredictor(HaikuBasePredictor):
+    """
+    Return haiku as a single string.
+    """
     def predict(self, 
-      seed: int = Input(description="A seed to always return the same result (optional, defaults to a random integer)", ge=0, default=None),
-      progressive: bool = Input(description="Yield haiku words one at a time, sleepin between each word", default=False),
-      sleep: float = Input(description="Time to sleep between each word (when using progressive output)", default=0.5),
-      ) -> Any:
-        if seed:
-          haiku = self.haikus[seed % len(self.haikus)]
-        else:
-          haiku = random.choice(self.haikus)
+      seed: int = Input(description="A seed to always return the same result (optional)", ge=0, default=None)
+      ) -> str:
+        haiku = self.get_haiku(seed)
+        return haiku
 
-        print("seed: ", seed)
-        print("progressive: ", progressive)
-        print("sleep: ", sleep)
+class ProgressivePredictor(HaikuBasePredictor):
+    """
+    Yield haiku as progressive output, one word at a time.
+    """
+    def predict(self, 
+      seed: int = Input(description="A seed to always return the same result (optional)", ge=0, default=None),
+      sleep: float = Input(description="Time to sleep between each word (when using progressive output)", default=0.1),
+      ) -> Generator[str, None, None]:
 
-        if progressive:
-          for word in haiku.split():
-            yield word
-            time.sleep(sleep)
-        else:
-          return haiku
+        haiku = self.get_haiku(seed)
+
+        for word in haiku.split():
+          yield word
+          time.sleep(sleep)
--- test_predict.py
+++ test_predict.py
@@ -1,21 +1,42 @@
-from predict import Predictor
+from predict import StandardPredictor, ProgressivePredictor
 
-def test_seed_always_returns_same_haiku():
-  p = Predictor()
-  p.setup()
-  output = p.predict(seed=123)
-  assert "pear blossoms" in output
 
-  output = p.predict(seed=123)
-  assert "pear blossoms" in output
+class TestStandardPredictor:
+  def test_seed_always_returns_same_haiku(self):
+    p = StandardPredictor()
+    p.setup()
+    output = p.predict(seed=123)
+    assert "pear blossoms" in output
 
-def test_without_seed_returns_random_haiku():
-  p = Predictor()
-  p.setup()
-  output1 = p.predict(seed=None)
-  assert isinstance(output1, str)
+    output = p.predict(seed=123)
+    assert "pear blossoms" in output
 
-  output2 = p.predict(seed=None)
-  assert isinstance(output2, str)
+  def test_without_seed_returns_random_haiku(self):
+    p = StandardPredictor()
+    p.setup()
+    output1 = p.predict(seed=None)
+    assert isinstance(output1, str)
 
-  assert output1 != output2
+    output2 = p.predict(seed=None)
+    assert isinstance(output2, str)
+
+    assert output1 != output2
+
+class TestProgressivePredictor:
+  def test_seed_always_returns_same_haiku(self):
+    p = ProgressivePredictor()
+    p.setup()
+    output = [result for result in p.predict(seed=123, sleep=0.1)]
+    assert(len(output) == 9)
+    assert(" ".join(output) == "from somewhere pear blossoms a curve in the road")
+
+  def test_without_seed_returns_random_haiku(self):
+    p = ProgressivePredictor()
+    p.setup()
+    output1 = [result for result in p.predict(sleep=0.01)]
+    assert(len(output1) > 5)
+
+    output2 = [result for result in p.predict(sleep=0.01)]
+    assert(len(output2) > 5)
+    
+    assert output1 != output2
