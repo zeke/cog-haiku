43b5f06a56c13b6ab057f5dca34ee34ece7b8687
Zeke Sikelianos <zeke@sikelianos.com>
2022-03-10 14:32:19 -0800

    add ProgressiveImagePredictor (#10)

--- predict.py
+++ predict.py
@@ -78,3 +78,29 @@ class ImagePredictor(HaikuBasePredictor):
 
         image_path = self.generate_image(haiku, color=color)
         return image_path
+
+
+class ProgressiveImagePredictor(HaikuBasePredictor):
+    """
+    Yield haiku as progressive output images, one word at a time.
+    """
+    def predict(self, 
+      seed: int = Input(description="A seed to always return the same result (optional)", ge=0, default=None),
+      sleep: float = Input(description="Time to sleep between each word (when using progressive output)", default=0.1),
+      source_image: Path = Input(description="An image from which to derive a background color for the output image (optional)", default=None),
+      ) -> Iterator[Path]:
+
+        haiku = self.get_haiku(seed)
+
+        # extract dominant color from source image for use in output image
+        if source_image and type(source_image) == str:
+          color = ColorThief(source_image).get_color(quality=1)
+        else:
+          color = None
+
+        words = haiku.split(" ")
+        for i,_word in enumerate(words):
+          haiku_so_far = " ".join(words[0:i+1])
+          image_path = self.generate_image(haiku_so_far, color=color)
+          yield Path(image_path)
+          time.sleep(sleep)
